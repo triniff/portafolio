798cdb5308a2e11264ade703acee9191193569c4
“Trinidad <tfuentesfuentealba@gmail.com>
2017-08-07 22:47:27 -0300

    editando completamente portafolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,32 @@
 $(document).ready(function(){
-    $(".push_menu").click(function(){
-         $(".wrapper").toggleClass("active");
+	window.addEventListener('scroll', function () {
+   const distanceY = window.pageYOffset || document.documentElement.scrollTop,
+   shrinkOn = 600,
+   headerEl = document.getElementsByTagName("nav")[0];
 
-         $("body").click(function(){
-    	 $(".wrapper").toggleClass("active");
-    })
+   if (distanceY > shrinkOn) {
+     headerEl.classList.add("scroll-menu");
+   } else {
+     headerEl.classList.remove("scroll-menu");
+   }
+
     });
 
-    (function ($) { 
-		$('.tab ul.tabs').addClass('active').find('> li:eq(0)').addClass('current');
-		
-		$('.tab ul.tabs li a').click(function (g) { 
-			var tab = $(this).closest('.tab'), 
-				index = $(this).closest('li').index();
-			
-			tab.find('ul.tabs > li').removeClass('current');
-			$(this).closest('li').addClass('current');
-			
-			tab.find('.tab_content').find('div.tabs_item').not('div.tabs_item:eq(' + index + ')').slideUp();
-			tab.find('.tab_content').find('div.tabs_item:eq(' + index + ')').slideDown();
-			
-			g.preventDefault();
-		} );
-	})(jQuery);
-});
+     /* Click listener on li items. i.e. when an li item is clicked, the following code is triggered */
+     $('#mainNavbar li').click(function(e) {
+          var $this = $(this);
 
+          /* Set the appropriate style for the active item */          
+          if (!$this.hasClass('active')) {
+               $('ul.nav li').removeClass('active');
+               $this.addClass('active');
+          }
+
+          /* Hide the navbar-collapse after that it is clicked */
+          $(".navbar-collapse").collapse('hide'); 
+     });
+
+     // bootstrap scrollspy function to go to the clicked and specific page section
+     $('body').scrollspy({ target: '.navbar'});
+
+});
